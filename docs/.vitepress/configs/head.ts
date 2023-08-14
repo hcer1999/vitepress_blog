e17cfd180b4e78063a4ec2d7b46ef861b0be842e
Hcer <bingkelele007@163.com>
2023-08-14 23:00:12 +0800

    fix: 修复搜索以及链接跳转功能

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -8,7 +8,7 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
-  ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
+  // ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
